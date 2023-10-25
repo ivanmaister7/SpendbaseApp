f49ca651516bb606021e1e7a99faae7cd626c142
Ivan Maister <ivanmaister7@gmail.com>
2023-10-25 13:58:04 +0300

    Initial Commit

---
+++ Spendbase/SpendbaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendbaseApp.swift
+//  Spendbase
+//
+//  Created by Master on 25.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpendbaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
